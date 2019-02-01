90b7052a7c68be26c6a01bd0c97f4ed3b2153971
JustinAndrade <j.andrade.john@gmail.com>
2019-02-01 01:47:15 -0500

    mouseover and drag elements added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+// button mouseover
+let button = document.querySelector('.btn');
+
+button.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'yellow';
+    event.target.style.color = 'black';
+});
+
+// image drag
+// allows the bus to show in the nav once you drag it
+
+let image = document.querySelector('img');
+
+image.addEventListener('drag', (event) => {
+    event.target.style.opacity = .5;
+});
